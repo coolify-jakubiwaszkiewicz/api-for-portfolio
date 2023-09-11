2412965f6f5cbeeaa53b8da7a0f1e7cd370e8aef
jakubiwaszkiewicz <102925580+jakubiwaszkiewicz@users.noreply.github.com>
2023-09-11 10:32:01 +0200

    update server.js

--- config/server.js
+++ config/server.js
@@ -7,4 +7,7 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  proxy: true,
+  url: 'https://api-portfolio.iw4sz.pl',
+  emitErrors: true,
 });
