e3c46c31baf5740e17077d1b75bbfbe28e1a125d
jakubiwaszkiewicz <102925580+jakubiwaszkiewicz@users.noreply.github.com>
2023-09-11 04:39:20 +0200

    update database.js

--- config/database.js
+++ config/database.js
@@ -1,18 +1,14 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
+    // strapi-api/config/database.js
+    module.exports = ({ env }) => ({
+      connection: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapidb'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'your_password_here'),
-        schema: 'public',
-      },
-      options: {},
-    },
-  },
-});
+        connection: {
+          host: env('DATABASE_HOST', 'localhost'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'strapi'),
+          user: env('DATABASE_USERNAME', 'postgres'),
+          password: env('DATABASE_PASSWORD', 'haslo'),
+          schema: env('DATABASE_SCHEMA', 'public'), // Not required
+        },
+      }
+    });
